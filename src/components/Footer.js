201e6322463107c2c1fb082f04ddae6b6a3cdc79
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 11:17:52 -0700

    footer component created

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+// Do I need to import anything for my footer?
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Footer Hooter Here</p>
+      <p>Maybe add my contact links here?</p>
+    </footer>
+  );
+};
+
+export default Footer;
