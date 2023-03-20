60d35836b8270238ab4c35b68f474448ea9cfc08
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 11:44:05 -0700

    started form - incomplete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
-// COPIED FROM ACTIVITY 15 - have not reviewed it yet
-
+// COPIED FROM ACTIVITY 15 - have not fully reviewed it yet
 import React, { useState } from "react";
 
 function Form() {
@@ -7,29 +6,31 @@ function Form() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
 
+  // ME: Adding email and message input for the form
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
-    const { name, value } = e.target;
+    // ME: Added to get value of email and message
+    const { name, value, email, message } = e.target;
 
     // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-    return name === "firstName" ? setFirstName(value) : setLastName(value);
+    // ME: Dont think I need this since I am not rendering the hello text at top of form like activity did - REWRITE IF ELSE STATEMENT HERE
+    // return name === "firstName" ? setFirstName(value) : setLastName(value);
   };
 
   const handleFormSubmit = (e) => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // Alert the user their first and last name, clear the inputs
-    alert(`Hello ${firstName} ${lastName}`);
+    // ME: removed aler - Alert the user their first and last name, clear the inputs
     setFirstName("");
     setLastName("");
   };
 
   return (
     <div>
-      <p>
-        Hello {firstName} {lastName}
-      </p>
       <form className="form">
         <input
           value={firstName}
@@ -44,6 +45,21 @@ function Form() {
           onChange={handleInputChange}
           type="text"
           placeholder="Last Name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        {/* Will have to add more input text box for message option */}
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
         />
         <button type="button" onClick={handleFormSubmit}>
           Submit
