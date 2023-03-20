89763a57cd9aa4d5d86d62ace9a250efca6ca310
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 11:07:35 -0700

    started About.js

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+// Import the components from AboutMe and Header and FOOTER(UNCREATED)
+import AboutMe from "../components/AboutMe";
+import PageHeader from "../components/PageHeader";
+import Footer from "../components/Footer"; //Dont forget to name Footer.js
+
+const AboutMe = () => {
+  return (
+    <section>
+      <PageHeader title="About Mariah" description="Nice to meet you!" />
+      {/* Will it render the entire About Me just putting it like below */}
+      <AboutMe />
+    </section>
+  );
+};
+
+export default About;
