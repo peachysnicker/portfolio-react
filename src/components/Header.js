38a7577d4fff0f882db36fc7d82f5269cd48a788
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 10:41:22 -0700

    Header.js created

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// Import for navLinks (uncreated)
+import NavLinks from "./NavLinks";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>This is my header</h1>
+      <NavLinks />
+    </header>
+  );
+};
+
+export default Header;
