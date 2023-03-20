df1a39a365079accca436c957234c4583caab457
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 10:38:25 -0700

    added index and app.js

---
+++ App.js
@@ -0,0 +1,6 @@
+import Header from "./components/Header";
+
+// What will be my main component to reference other React components?
+function App() {}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// Will have to import my pages I create
+
+import App from "./App";
+
+//First argument is the component we want to render, and the second is the container element on the page
+ReactDOM.render(<App />, document.getElementById("root"));
