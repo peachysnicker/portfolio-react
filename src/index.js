7b67c17501ff59dd5e51abfd07519d317bc7075c
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 11:29:36 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // Will have to import my pages I create
+import "./pages/about";
 
-import App from "./App";
+import App from "../App";
 
 //First argument is the component we want to render, and the second is the container element on the page
 ReactDOM.render(<App />, document.getElementById("root"));
