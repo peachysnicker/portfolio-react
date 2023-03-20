83c6cd7ffee9d7f5906fa6b5fc6b98531b20dd81
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-20 11:15:38 -0700

    about me component created

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+// Nothing to import for now - possibly images later
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h3>This is my About Me Component!</h3>
+      <h3>My Name is Mariah</h3>
+      <h5> blah blah blah blah </h5>
+    </div>
+  );
+};
+
+export default AboutMe;
