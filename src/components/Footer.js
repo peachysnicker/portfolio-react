b5d64108a3feebdb3ace2148fbb552408c946d84
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-04-09 23:53:23 -0700

    footer socials

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,39 @@ const Footer = () => {
   return (
     <footer>
       <div className="footerContainer">
-        <a className="emailLink" href="mailto:feser.mk@gmail.com">
+        <navigator className="nav">
+          <a
+            className="nav-item col-4 d-flex justify-content-center"
+            href="https://github.com/peachysnicker"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src="./images/github-icon-white.png" alt="Github icon" />
+          </a>
+          <a
+            className="nav-item col-4 d-flex justify-content-center"
+            href="https://www.linkedin.com/in/mariah-feser-ab8250243/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src="./images/linked-in-icon-white.png" alt="Linked In icon" />
+          </a>
+          <a
+            className="nav-item col-4 d-flex justify-content-center"
+            href="https://www.instagram.com/microbeautystudio/?hl=en"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img src="./images/instagram-white-icon.png" alt="Instagram Icon" />
+          </a>
+          <p className="col-12 pt-3 d-flex justify-content-center">
+            Created with ♥ by Mariah Feser
+          </p>
+          <p className="col-12  d-flex justify-content-center"> © 2023</p>
+        </navigator>
+        {/* <a className="emailLink" href="mailto:feser.mk@gmail.com">
           📧
-        </a>
-        <p className="footerText">Created with ♥ by Mariah Feser</p>
-        <p className="footerSubText"> © 2023</p>
+        </a> */}
       </div>
     </footer>
   );
